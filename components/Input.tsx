893cd6862c48861fffc6a0909c97c4c1173635ee
AmrViernes <amraromoro@hotmail.com>
2024-05-28 14:53:40 +0200

    feat: added input component

---
+++ components/Input.tsx
@@ -0,0 +1,43 @@
+import { StyleSheet, TextInput, View } from 'react-native';
+import React from 'react';
+import { tintColorDark, tintColorLight } from '../constants/Colors';
+
+type Props = TextInput['props'];
+const Input = (props: Props) => {
+  const { ...otherProps } = props;
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        {...otherProps}
+        autoComplete='additional-name'
+      />
+    </View>
+  );
+};
+
+export default Input;
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+  },
+  input: {
+    backgroundColor: tintColorLight,
+    color: '#fff',
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    borderRadius: 10,
+    borderColor: tintColorDark,
+    width: 350,
+    shadowColor: '#fff',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 1,
+    elevation: 4,
+  },
+});
