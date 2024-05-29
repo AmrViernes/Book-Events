3b305ab204b02a9cf7373a2ff8cac883a3e5961f
AmrViernes <amraromoro@hotmail.com>
2024-05-29 18:43:33 +0300

    structure: added home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
 import { StyleSheet, Platform } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import Explore from '../ExploreScreen';
+import Explore from '@/app/Home';
 
 export default function HomeScreen() {
   return (
