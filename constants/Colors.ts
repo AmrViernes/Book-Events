7c7f537458efb3b9ef34d0a290313a3cec7dd341
AmrViernes <amraromoro@hotmail.com>
2024-05-29 18:48:27 +0300

    refactor: the colors const's

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,21 +1,22 @@
-export const tintColorLight = '#2B2B2B';
-export const tintColorDark = '#fff';
+export const darkColor = '#2B2B2B';
+export const lightColor = '#fff';
+export const goldColor = '#D6B560';
 
 export const Colors = {
   light: {
     text: '#11181C',
     background: '#fff',
-    tint: tintColorLight,
+    tint: goldColor,
     icon: '#687076',
-    tabIconDefault: '#D6B560',
-    tabIconSelected: tintColorLight,
+    tabIconDefault: goldColor,
+    tabIconSelected: darkColor,
   },
   dark: {
     text: '#ECEDEE',
     background: '#181818',
-    tint: tintColorDark,
+    tint: goldColor,
     icon: '#9BA1A6',
-    tabIconDefault: '#D6B560',
-    tabIconSelected: tintColorDark,
+    tabIconDefault: goldColor,
+    tabIconSelected: lightColor,
   },
 };
