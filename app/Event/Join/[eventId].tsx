451ee131c47c4a9c05848ffcfadcce50aa554174
AmrViernes <amraromoro@hotmail.com>
2024-05-30 15:39:58 +0300

    feat: added the initial join event screen

---
+++ app/Event/Join/[eventId].tsx
@@ -0,0 +1,106 @@
+import StackScreen from '@/components/Stacks/StackScreen';
+import { lightColor } from '@/constants/Colors';
+import { Feather } from '@expo/vector-icons';
+import { Image } from 'expo-image';
+import React from 'react';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import JoinEventForm from '../components/JoinEventForm';
+
+const JoinUsScreen = () => {
+  return (
+    <ScrollView style={{ height: '100%' }} showsVerticalScrollIndicator={false}>
+      <StackScreen title='Join Us' />
+
+      <View style={styles.container}>
+        <Text style={styles.aboutText}>
+          Request to join “The grill house restaurant”
+        </Text>
+        <Text style={styles.aboutEventDescription}>
+          Please fill in the information bellow so our expert team can connect
+          you and setup your membership.
+        </Text>
+
+        <View style={styles.imageContainer}>
+          <Image
+            source={require('@/assets/images/photos/1.png')}
+            contentFit='cover'
+            style={{ width: '100%', height: 150 }}
+          />
+        </View>
+
+        <View style={styles.dateContainer}>
+          <View style={styles.dateSubContainer}>
+            <Feather name='calendar' size={24} color='white' />
+            <Text style={styles.dateText}>
+              23 May - Jul 24, 2024 | 09:45 AM
+            </Text>
+          </View>
+          <View style={styles.dateSubContainer}>
+            <Feather name='map-pin' size={24} color='white' />
+            <Text style={styles.dateText}>
+              Cairo, Egypt
+            </Text>
+          </View>
+        </View>
+
+      <View style={{ width: '100%' }}>
+        <JoinEventForm />
+      </View>
+      </View>
+    </ScrollView>
+  );
+};
+
+export default JoinUsScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    alignSelf: 'center',
+    justifyContent: 'center',
+    marginVertical: 10,
+    paddingTop: 20,
+    width: '90%',
+    height: 'auto',
+  },
+  aboutText: {
+    color: lightColor,
+    fontWeight: 'bold',
+    fontSize: 18,
+    marginVertical: 10,
+  },
+  aboutEventDescription: {
+    color: lightColor,
+    fontSize: 16,
+  },
+  imageContainer: {
+    borderWidth: 0.3,
+    borderColor: lightColor,
+    borderRadius: 10,
+    width: '100%',
+    height: 'auto',
+    overflow: 'hidden',
+    marginVertical: 30,
+  },
+  dateContainer: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 10,
+    width: '98%',
+    height: 'auto',
+  },
+  dateSubContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignSelf: 'flex-start',
+    gap: 10,
+  },
+  dateText: {
+    color: lightColor,
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+});
