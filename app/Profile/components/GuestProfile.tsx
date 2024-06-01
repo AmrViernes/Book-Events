6b69180429111fc215c34437afed10c6d303e0f3
AmrViernes <amraromoro@hotmail.com>
2024-06-01 16:01:52 +0300

    feat: finished the component

---
+++ app/Profile/components/GuestProfile.tsx
@@ -0,0 +1,57 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import GoldButton from '@/components/buttons/GoldButton';
+import { lightColor } from '@/constants/Colors';
+import ProfileButtonsMenu from './ProfileButtonsMenu';
+
+const GuestProfile = () => {
+  return (
+    <View style={styles.container}>
+      <View
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          alignContent: 'center',
+        }}
+      >
+        <Text style={styles.title}>Welcome to VVIP</Text>
+        <Text style={styles.description}>
+          Enjoy a special experience with VVIP!
+        </Text>
+        <Text style={styles.description}>Join us now!</Text>
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <GoldButton title='Join us now' onPress={() => {}} />
+      </View>
+
+      <ProfileButtonsMenu isLoggedIn={false}/>
+    </View>
+  );
+}
+
+export default GuestProfile
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',  
+  },
+  buttonContainer: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignSelf: 'center',
+    width: '90%',
+    marginTop: 50,
+  },
+  title: {
+    fontSize: 24,
+    color: lightColor,
+    paddingVertical: 20,
+  },
+  description: {
+    fontSize: 16,
+    color: lightColor,
+  },
+});
