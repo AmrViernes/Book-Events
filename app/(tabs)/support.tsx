9524ff40d97c70c837b45eee3d406687e980770d
AmrViernes <amraromoro@hotmail.com>
2024-06-01 15:58:49 +0300

    refactor: import

--- app/(tabs)/support.tsx
+++ app/(tabs)/support.tsx
@@ -1,7 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
-import SupportScreen from '../SupportScreen'
+import SupportScreen from '../Support'
 
 const support = () => {
   return (
