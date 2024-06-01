795297b35e833a59df7b70906856b7dc47e404bd
AmrViernes <amraromoro@hotmail.com>
2024-06-01 15:58:12 +0300

    refactor: import

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,7 +1,7 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
-import ProfileScreen from '../ProfileScreen'
+import ProfileScreen from '@/app/Profile'
 
 const profile = () => {
   return (
