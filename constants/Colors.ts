d892102d5897891174839e619428543c95e3b7c1
AmrViernes <amraromoro@hotmail.com>
2024-06-01 16:03:03 +0300

    feat: added new colors

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,12 +1,14 @@
 export const darkColor = '#2B2B2B';
+export const backgroundColor = '#181818';
 export const lightColor = '#fff';
+export const lightGrayColor = '#ccc';
 export const goldColor = '#D6B560';
 export const darkGrayColor = '#2B2B2B';
 
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
+    text: '#ECEDEE',
+    background: darkColor,
     tint: goldColor,
     icon: '#687076',
     tabIconDefault: goldColor,
@@ -14,7 +16,7 @@ export const Colors = {
   },
   dark: {
     text: '#ECEDEE',
-    background: '#181818',
+    background: darkColor,
     tint: goldColor,
     icon: '#9BA1A6',
     tabIconDefault: goldColor,
