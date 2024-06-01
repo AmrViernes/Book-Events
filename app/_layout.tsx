ac4f78f8f13b67a445d1ddff5acd703e85eb133a
AmrViernes <amraromoro@hotmail.com>
2024-06-01 15:56:52 +0300

    refactor: model import

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,7 +10,7 @@ import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
-import { ModalProvider } from './context/ModelContext';
+import { ModalProvider } from '../context/ModelContext';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
