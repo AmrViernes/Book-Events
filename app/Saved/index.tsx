731484af1dd627c21a525a38b4e5ace1e8d4207a
AmrViernes <amraromoro@hotmail.com>
2024-06-02 14:17:29 +0300

    feat: added empty saved

--- app/Saved/index.tsx
+++ app/Saved/index.tsx
@@ -1,14 +1,28 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import NoSaved from './components/NoSaved';
 
-const SavedScreen = () => {
+const SavedEventsScreen = () => {
   return (
     <View>
-      <Text>SavedScreen</Text>
+      {'noSavedItems' !== 'noSavedItems' && (
+        <View style={styles.container}>
+          <NoSaved />
+        </View>
+      )}
     </View>
-  )
+  );
 }
 
-export default SavedScreen
+export default SavedEventsScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    height: '100%',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+  },
+});
