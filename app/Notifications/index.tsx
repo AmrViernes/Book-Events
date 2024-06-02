024e47eeec6cb37b8f08af3943496ca02da82049
AmrViernes <amraromoro@hotmail.com>
2024-06-02 14:21:54 +0300

    refactor: add flash list and moved data to dummy

--- app/Notifications/index.tsx
+++ app/Notifications/index.tsx
@@ -1,6 +1,6 @@
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
-import StackScreen from '@/components/Stacks/StackScreen';
+import StackScreen from '@/components/stacks/StackScreen';
 import NoNotifications from './components/NoNotifications';
 import NotificationItem from './components/NotificationItem';
 import {
@@ -8,55 +8,13 @@ import {
   GestureHandlerRootView,
 } from 'react-native-gesture-handler';
 import DeleteNotificationButton from './components/DeleteNotificationButton';
+import { FlashList } from '@shopify/flash-list';
+import { notificationsData } from '@/constants/dummy';
 
-const NoNotificationsData = [
-  {
-    icon: 'calendar',
-    title: 'Upcoming event',
-    message: '“City Meeting” is coming soon!',
-    date: '3 minutes ago',
-  },
-  {
-    icon: 'check',
-    title: 'Success message',
-    message: 'Your request to join was sent successfully',
-    date: '2 hours ago',
-  },
-  {
-    icon: 'check',
-    title: 'Success message',
-    message: 'Your request to join was sent successfully',
-    date: '1 week ago',
-  },
-  {
-    icon: 'calendar',
-    title: 'Upcoming event',
-    message: "Book Event” is coming soon! Don't miss out!",
-    date: '25 days later',
-  },
-  {
-    icon: 'check',
-    title: 'Success message',
-    message: 'Your request to join was sent successfully',
-    date: '7 hours ago',
-  },
-  {
-    icon: 'calendar',
-    title: 'Upcoming event',
-    message: "“Party at Hilton” is coming soon! Don't miss out!",
-    date: '4 days later',
-  },
-  {
-    icon: 'check',
-    title: 'Success message',
-    message: 'Your request to join was sent successfully',
-    date: '7 hours ago',
-  },
-];
 const index = () => {
   return (
     <View style={styles.scroll}>
-      <StackScreen title='Notifications' showHeaderRight={true}/>
+      <StackScreen title='Notifications' showHeaderRight={true} />
       {'noNotifications' !== 'noNotifications' && (
         <View style={styles.container}>
           <NoNotifications />
@@ -64,20 +22,21 @@ const index = () => {
       )}
       <ScrollView style={styles.scroll}>
         <GestureHandlerRootView>
-          {NoNotificationsData.map((item, index) => (
-            <Swipeable
-              key={index}
-              renderRightActions={() => DeleteNotificationButton()}
-            >
-              <NotificationItem
-                key={index}
-                icon={item.icon}
-                title={item.title}
-                message={item.message}
-                date={item.date}
-              />
-            </Swipeable>
-          ))}
+          <FlashList
+            data={notificationsData}
+            renderItem={({ item }) => (
+              <Swipeable renderRightActions={() => DeleteNotificationButton()}>
+                <NotificationItem
+                  icon={item.icon}
+                  title={item.title}
+                  message={item.message}
+                  date={item.date}
+                />
+              </Swipeable>
+            )}
+            estimatedItemSize={110}
+            keyExtractor={(item) => String(item.id)}
+          />
         </GestureHandlerRootView>
       </ScrollView>
     </View>
