432fc0e27aa260478845718cfb458a7234b5b096
AmrViernes <amraromoro@hotmail.com>
2024-06-02 14:14:49 +0300

    feat: added dummy data

---
+++ constants/dummy.ts
@@ -0,0 +1,132 @@
+export const events = [
+  {
+    title: 'Stars hotel',
+    image: require('@/assets/images/photos/history1.svg'),
+    date: '12 Jan  |  09:45 AM',
+    requestStatus: 'Pending',
+  },
+  {
+    title: 'New home',
+    image: require('@/assets/images/photos/history2.svg'),
+    date: '22 Aug  |  09:45 AM',
+    requestStatus: 'Completed',
+  },
+  {
+    title: 'Swimming',
+    image: require('@/assets/images/photos/history3.svg'),
+    date: '22 Aug  |  09:45 AM',
+    requestStatus: 'Pending',
+  },
+  {
+    title: 'Technology event',
+    image: require('@/assets/images/photos/history4.svg'),
+    date: '22 Aug  |  09:45 AM',
+    requestStatus: 'Completed',
+  },
+  {
+    title: 'VIP event',
+    image: require('@/assets/images/photos/history5.svg'),
+    date: '22 Aug  |  09:45 AM',
+    requestStatus: 'Pending',
+  },
+  {
+    title: 'Art competition',
+    image: require('@/assets/images/photos/history6.svg'),
+    date: '25 Dec  |  09:45 AM',
+    requestStatus: 'Completed',
+  },
+  {
+    title: ' New sport',
+    image: require('@/assets/images/photos/history7.svg'),
+    date: '11 May  |  09:45 AM',
+    requestStatus: 'Completed',
+  },
+  {
+    title: 'Sea view hotel',
+    image: require('@/assets/images/photos/history8.svg'),
+    date: '22 Aug  |  09:45 AM',
+    requestStatus: 'Pending',
+  },
+  {
+    title: 'Family restaurant',
+    image: require('@/assets/images/photos/history9.svg'),
+    date: '22 Aug  |  09:45 AM',
+    requestStatus: 'Completed',
+  },
+];
+
+export const homeMainEvents = [
+  {
+    title: 'New home',
+    image: require('@/assets/images/photos/1.png'),
+    dayOfMonth: 22,
+    month: 'Aug',
+    location: 'Cairo, Egypt',
+  },
+  {
+    title: 'Stars hotel',
+    image: require('@/assets/images/photos/2.png'),
+    dayOfMonth: 12,
+    month: 'Jan',
+    location: 'London, UK',
+  },
+  {
+    title: 'Swimming',
+    image: require('@/assets/images/photos/3.png'),
+    dayOfMonth: 10,
+    month: 'May',
+    location: 'Paris, France',
+  },
+];
+
+export const notificationsData = [
+  {
+    id: 1,
+    icon: 'calendar',
+    title: 'Upcoming event',
+    message: '“City Meeting” is coming soon!',
+    date: '3 minutes ago',
+  },
+  {
+    id: 2,
+    icon: 'check',
+    title: 'Success message',
+    message: 'Your request to join was sent successfully',
+    date: '2 hours ago',
+  },
+  {
+    id: 3,
+    icon: 'check',
+    title: 'Success message',
+    message: 'Your request to join was sent successfully',
+    date: '1 week ago',
+  },
+  {
+    id: 4,
+    icon: 'calendar',
+    title: 'Upcoming event',
+    message: "Book Event” is coming soon! Don't miss out!",
+    date: '25 days later',
+  },
+  {
+    id: 5,
+    icon: 'check',
+    title: 'Success message',
+    message: 'Your request to join was sent successfully',
+    date: '7 hours ago',
+  },
+  {
+    id: 6,
+    icon: 'calendar',
+    title: 'Upcoming event',
+    message: "“Party at Hilton” is coming soon! Don't miss out!",
+    date: '4 days later',
+  },
+  {
+    id: 7,
+    icon: 'check',
+    title: 'Success message',
+    message: 'Your request to join was sent successfully',
+    date: '7 hours ago',
+  },
+];
