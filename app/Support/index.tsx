1e22737a9ab23f21fd02844d161760b22285a93d
AmrViernes <amraromoro@hotmail.com>
2024-06-03 18:57:04 +0300

    refactor: added new wrapper

--- app/Support/index.tsx
+++ app/Support/index.tsx
@@ -1,46 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import ContactCard from './components/ContactCard';
+import LogoCard from '@/components/cards/LogoCard';
 
 const SupportScreen = () => {
   return (
-    <View style={styles.container}>
-      <View></View>
-      <View style={styles.cardsContainer}>
-        <ContactCard
-          icon='phone'
-          title='Phone Number'
-          message='+20 012 345 6789'
-        />
-        <ContactCard
-          icon='envelope-o'
-          title='Email Address'
-          message='example@example.com'
-        />
-        <ContactCard
-          icon='whatsapp'
-          title='WhatsApp'
-          message='+20 012 345 6789'
-        />
-      </View>
-    </View>
+    <LogoCard
+      title='Hello ,How can we help you ?'
+      description='We are always happy to help. Please feel free to contact us if you
+            need anything.'
+    >
+      <ContactCard
+        icon='phone'
+        title='Phone Number'
+        message='+20 012 345 6789'
+      />
+      <ContactCard
+        icon='envelope-o'
+        title='Email Address'
+        message='example@example.com'
+      />
+      <ContactCard
+        icon='whatsapp'
+        title='WhatsApp'
+        message='+20 012 345 6789'
+      />
+    </LogoCard>
   );
 };
 
 export default SupportScreen;
-
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  cardsContainer: {
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '100%',
-    height: '100%',
-    paddingTop: 50,
-  },
-});
