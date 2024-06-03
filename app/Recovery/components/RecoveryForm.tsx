5832ca6ea607bfb88127c01670a647a14c2c938e
AmrViernes <amraromoro@hotmail.com>
2024-06-03 18:55:22 +0300

    feat: added initial recovery

---
+++ app/Recovery/components/RecoveryForm.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const RecoveryForm = () => {
+  return (
+    <SafeAreaView>
+      <Text>RecoveryForm</Text>
+    </SafeAreaView>
+  )
+}
+
+export default RecoveryForm
+
+const styles = StyleSheet.create({})
