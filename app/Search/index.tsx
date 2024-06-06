7f54329ac546faf0c16cec75090efea8a81011d8
AmrViernes <amraromoro@hotmail.com>
2024-06-06 15:32:26 +0300

    fix : css

--- app/Search/index.tsx
+++ app/Search/index.tsx
@@ -4,17 +4,30 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import StackScreen from '@/components/stacks/StackScreen';
 import SearchInput from '@/components/inputs/SearchInput';
 import CategoriesList from './components/CategoriesList';
+import { lightColor } from '@/constants/Colors';
 
 const SearchScreen = () => {
   return (
     <SafeAreaView>
       <StackScreen title='Search' />
-      <SearchInput />
-      <CategoriesList />
+      <View style={styles.searchContainer}>
+        <SearchInput />
+        <View>
+          <Text style={{ color: lightColor, fontSize: 18, paddingLeft: 15, paddingBottom: 10 }}>
+            Search by category
+          </Text>
+          <CategoriesList />
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
 
 export default SearchScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  searchContainer: {
+    flexDirection: 'column',
+    gap: 30,
+  },
+});
