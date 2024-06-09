56477bf16a94e8fe3f04aebe431b7cd2ecd3456b
AmrViernes <amraromoro@hotmail.com>
2024-06-09 15:27:11 +0300

    fix: stats bar color

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,13 @@
-import { StyleSheet, Platform } from 'react-native';
+import { StyleSheet, Platform, StatusBar, useColorScheme } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import ExploreScreen from '@/app/Home';
 
 export default function HomeScreen() {
+    const colorScheme = useColorScheme();
+    
   return (
     <SafeAreaProvider>
+      <StatusBar barStyle='light-content' />
       <ExploreScreen />
     </SafeAreaProvider>
   );
